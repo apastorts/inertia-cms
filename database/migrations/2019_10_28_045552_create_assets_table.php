b383786819905814d4f61b0ea86e17891b2df29b
Abel Pastor Orts <abel.pastor@redhub.com.au>
2019-10-28 15:59:15 +1100

    Created Database Migrations

--- database/migrations/2019_10_28_045552_create_assets_table.php
+++ database/migrations/2019_10_28_045552_create_assets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAssetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::table('assets', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('avatar')->nullable();
-            $table->rememberToken();
+            $table->integer('size');
+            $table->string('type');
+            $table->integer('gallery_id')->default(0);
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('assets');
     }
 }
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ This project is planned to be a Laravel CMS created with Inertia.js.
 ## Road Map
 
 * [x] Prepare Environment
-* [ ] Create Database Migrations
+* [x] Create Database Migrations
 * [ ] Create Dashboard
 * [ ] Create Pages to Edit Components
 * [ ] Create Assets Gallery
